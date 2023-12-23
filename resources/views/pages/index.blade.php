817304bcf7f700e6447607748cd982cd6a3c4c2b
Hassan Kerdash <146722096+HassanKerdash-Qutell@users.noreply.github.com>
2023-12-23 20:39:12 +0200

    add page of challenge yourself

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,8 +5,16 @@
 @section('content')
 <div class="img-btn">
     <img class="red-small-btn" src="{{asset('website/images/red-small-btn.svg')}}" alt="" />
+
+    @auth
+    <h2>مرحبا {{ auth()->user()->name }}!</h2>
+    @endauth
+
+    @guest
     <a href="#">
         <h2>تحد نفسك</h2>
-    </a>
+    </a>   
+    @endguest
+
 </div>
 @endsection
