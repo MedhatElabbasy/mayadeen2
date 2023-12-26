456707c19547879db2516fba9b095770b71ba44c
3bsamad0x90 <mohamedabdelsamad122@gmail.com>
2023-12-26 23:54:43 +0200

    dates of poems, create migration file and his model, and then create filament resource to make crud operations

---
+++ app/Models/DatesOfPoem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DatesOfPoem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'owner',
+        'date',
+        'start_time',
+        'end_time',
+        'details',
+    ];
+
+    
+}
