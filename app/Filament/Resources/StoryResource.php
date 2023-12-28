f24bc46889cc6f00a0f363b28475b2a47b08b24f
Hassan <kerdashhassan@gmail.com>
2023-12-28 10:59:42 +0200

    up

--- app/Filament/Resources/StoryResource.php
+++ app/Filament/Resources/StoryResource.php
@@ -31,35 +31,100 @@ class StoryResource extends Resource
             ->schema([
                 Forms\Components\Card::make()
                     ->schema([
-                        Forms\Components\TextInput::make('title')
-                            ->label('الإسم')
-                            ->placeholder('إسم الأقصوصة')
-                            ->required()
-                            ->minLength(3)
-                            ->maxLength(255)
-                            ->rules('required|min:3|max:255'),
-
-                        Forms\Components\RichEditor::make('content')
-                            ->label('المحتوي')
-                            ->placeholder('محتوي الأقصوصة')
-                            //->toolbarButtons(['bold', 'italic', 'link', 'clean'])
-                            ->required()
-                            ->minLength(10)
-                            ->rules('required|string'),
-                    ]),
-
-                    Forms\Components\Section::make("الإستطلاع")
-                    ->schema([
                         Forms\Components\Grid::make(1)
-                        ->schema([
-                            Forms\Components\Textarea::make('opinion')
-                                ->label('رأي المستطلع')
-                                ->placeholder('رأي المستطلع')
-                                ->required()
-                                ->rules('required'),
-                        ])
+                            ->schema([
+                                Forms\Components\TextInput::make('title')
+                                    ->label('الإسم')
+                                    ->placeholder('إسم الأقصوصة')
+                                    ->required()
+                                    ->minLength(3)
+                                    ->maxLength(255)
+                                    ->rules('required'),
+
+                                Forms\Components\RichEditor::make('content')
+                                    ->label('المحتوي')
+                                    ->placeholder('محتوي الأقصوصة')
+                                    //->toolbarButtons(['bold', 'italic', 'link', 'clean'])
+                                    ->required()
+                                    // ->minLength(10)
+                                    ->rules('required|string'),
+
+
+                                    Forms\Components\TextInput::make('w1_name')
+                                    ->label('الكاتب الأول')
+                                    ->placeholder('اسم الكاتب الأول')
+                                    ->required()
+                                    // ->minLength(2)
+                                    // ->maxLength(255)
+                                    ->rules('required'),
+
+                                Forms\Components\TextInput::make('w1_number')
+                                    ->label('رقم الكاتب الأول')
+                                    ->placeholder('رقم الكاتب الأول')
+                                    ->nullable()
+                                    // ->minLength(9)
+                                    // ->maxLength(20)
+                                    ->rules('nullable'),
+
+                                Forms\Components\TextInput::make('w1_email')
+                                    ->label('البريد الإلكتروني للكاتب الأول')
+                                    ->placeholder('بريد الكاتب الأول')
+                                    ->nullable()
+                                    ->email()
+                                    ->rules('nullable|email'),
+
+
+                                    ///
+
+                                    Forms\Components\TextInput::make('w2_name')
+                                    ->label('الكاتب الثاني')
+                                    ->placeholder('اسم الكاتب الثاني')
+                                    ->required()
+                                    // ->minLength(2)
+                                    // ->maxLength(255)
+                                    ->rules('required|min:2'),
+
+                                Forms\Components\TextInput::make('w2_number')
+                                    ->label('رقم الكاتب الثاني')
+                                    ->placeholder('رقم الكاتب الثاني')
+                                    ->nullable()
+                                    // ->minLength(9)
+                                    // ->maxLength(20)
+                                    ->rules('nullable'),
+
+                                Forms\Components\TextInput::make('w2_email')
+                                    ->label('البريد الإلكتروني للكاتب الثاني')
+                                    ->placeholder('بريد الكاتب الثاني')
+                                    ->nullable()
+                                    ->email()
+                                    ->rules('nullable|email'),
+
+                                    //////
+
+                                    Forms\Components\TextInput::make('w3_name')
+                                    ->label('الكاتب الثالث')
+                                    ->placeholder('اسم الكاتب الثالث')
+                                    ->required()
+                                    // ->minLength(2)
+                                    // ->maxLength(255)
+                                    ->rules('required'),
+
+                                Forms\Components\TextInput::make('w3_number')
+                                    ->label('رقم الكاتب الثالث')
+                                    ->placeholder('رقم الكاتب الثالث')
+                                    ->nullable()
+                                    // ->minLength(9)
+                                    // ->maxLength(20)
+                                    ->rules('nullable'),
+
+                                Forms\Components\TextInput::make('w3_email')
+                                    ->label('البريد الإلكتروني للكاتب الثالث')
+                                    ->placeholder('بريد الكاتب الثالث')
+                                    ->nullable()
+                                    ->email()
+                                    ->rules('nullable|email'),
+                            ]),
                     ]),
-                ])
             ]);
     }
 
@@ -108,16 +173,57 @@ class StoryResource extends Resource
                             ->hiddenLabel(),
                     ]),
 
-                 \Filament\Infolists\Components\Section::make('محتوي الأقصوصة')
-                 ->schema([
+                \Filament\Infolists\Components\Section::make('محتوي الأقصوصة')
+                    ->schema([
+                        \Filament\Infolists\Components\TextEntry::make('content')
+                            ->label('المحتوي')
+                            ->hiddenLabel()
+                            ->html(),
+                    ]),
 
-                \Filament\Infolists\Components\TextEntry::make('content')
-                    ->label('المحتوي')
-                    ->hiddenLabel()
-                    ->html(),
-                  ])
+                \Filament\Infolists\Components\Section::make(' الكاتب الأول')
+                    ->schema([
+                        \Filament\Infolists\Components\TextEntry::make('w1_name')
+                        ->label(' الأسم')
+                            ->html(),
+                            \Filament\Infolists\Components\TextEntry::make('w1_number')
+                            ->label('  الهاتف')
+                            ->html(),
+                        \Filament\Infolists\Components\TextEntry::make('w1_email')
+                            ->label('البريد الإلكتروني  ')
+                            ->html(),
+                    ]),
+
+                \Filament\Infolists\Components\Section::make('الكاتب الثاني')
+                    ->schema([
+                        \Filament\Infolists\Components\TextEntry::make('w2_name')
+                            ->label(' الاسم')
+                            ->html(),
+                        \Filament\Infolists\Components\TextEntry::make('w2_number')
+                            ->label('الهاتف  ')
+                            ->html(),
+                        \Filament\Infolists\Components\TextEntry::make('w2_email')
+                            ->label('البريد الإلكتروني  ')
+                            ->html(),
+                    ]),
+
+                \Filament\Infolists\Components\Section::make('الكاتب الثالث')
+                    ->schema([
+                        \Filament\Infolists\Components\TextEntry::make('w3_name')
+                            ->label(' الاسم')
+                            ->html(),
+                        \Filament\Infolists\Components\TextEntry::make('w3_number')
+                            ->label('الهاتف  ')
+
+                            ->html(),
+                        \Filament\Infolists\Components\TextEntry::make('w3_email')
+                            ->label('البريد الإلكتروني  ')
+                            ->html(),
+
+                    ]),
             ]);
     }
+
 
     public static function getRelations(): array
     {
