042e81123dfe4e04d5602dd5da54b7c32797245c
Hassan Kerdash <146722096+HassanKerdash-Qutell@users.noreply.github.com>
2024-01-03 20:34:29 +0200

    up

--- resources/views/pages/poems/table/fosha/index.blade.php
+++ resources/views/pages/poems/table/fosha/index.blade.php
@@ -73,7 +73,7 @@ $dates = computed(function () {
 
                 </div>
 
-                <div x-data="{ currentDate: 0 }">
+                <div wire:ignore x-data="{ currentDate: 0 }">
                     @foreach ($this->dates as $item)
                         <div x-data="{
                             showContent: false,
@@ -119,7 +119,7 @@ $dates = computed(function () {
 
                 <div class="flex flex-col justify-center items-center px-4">
                     @if(setting('shareYourPoemQrCode'))
-                    <img class="py-8 rounded-lg max-w-72 max-h-72" src="{{ url('storage/'.setting('shareYourPoemQrCode')) }}">
+                        <img class="py-8 rounded-lg max-w-72 max-h-72" src="{{ url('storage/'.setting('shareYourPoemQrCode')) }}">
                     @endif
 
                     <span class="text-3xl md:text-4xl text-center font-bold py-8">شاركنا قصيدتك بالفصحى !</span>
