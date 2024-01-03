78308a229d1cd56f99fe7fdadd9a82204e404a63
Hassan Kerdash <146722096+HassanKerdash-Qutell@users.noreply.github.com>
2024-01-04 01:38:00 +0200

    up

--- resources/views/pages/visitors/index.blade.php
+++ resources/views/pages/visitors/index.blade.php
@@ -95,9 +95,9 @@ $submit = function () {
 
                                 <div class="w-full">
                                     <label for="name" class="block mb-2 font-medium text-[#f1e1c6]">الصورة</label>
-
+                                
                                     <label for="uploadFile"
-                                        class="bg-[#f1e1c6] text-black text-base rounded h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
+                                           class="bg-[#f1e1c6] text-black text-base rounded h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-8 mb-2 fill-black" viewBox="0 0 32 32">
                                             <path
                                                 d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
@@ -107,7 +107,7 @@ $submit = function () {
                                                 data-original="#000000" />
                                         </svg>
                                         <span class="font-sans">رفع الصورة</span>
-                                        <input type="file" accept="image/*" id='uploadFile' class="hidden" wire:model="image" />
+                                        <input type="file" accept="image/*" id='uploadFile' class="w-full" wire:change="image" />
                                         <p class="text-xs text-gray-400 mt-2 font-sans">مسموح بالصورة فقط.</p>
                                         <div wire:loading wire:target="image" class="text-sm text-gray-500 italic mt-4">يتم الرفع ...</div>
                                         @if($this->image) <div class="text-center text-sm text-gray-500 italic mt-4">تم اختيار الصورة</div> @endif
