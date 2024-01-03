4630a6ce00fb4b8f2a42ba3d526506c9454f3042
Hassan Kerdash <146722096+HassanKerdash-Qutell@users.noreply.github.com>
2024-01-03 21:53:00 +0200

    up

--- resources/views/pages/poems/table/fosha/index.blade.php
+++ resources/views/pages/poems/table/fosha/index.blade.php
@@ -23,7 +23,7 @@ $dates = computed(function () {
 @section('content')
     @volt
         <div id="app" class="mb-4">
-            <div class="px-0 md:px-8">
+            <div class="px-0 md:px-4">
                 <div class="py-2 px-2 md:px-8 bg-[#ec6646]">
                     <img src="{{ asset('website/images/navbar-light.svg') }}" class="w-full min-h-12 min-w-12 md:w-auto mx-auto">
 
