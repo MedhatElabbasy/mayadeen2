9346a6c1dfd2b3e62388cfe977a9f6ba6c8d4ab0
Hassan Kerdash <146722096+HassanKerdash-Qutell@users.noreply.github.com>
2024-01-03 23:56:16 +0200

    up

--- resources/views/pages/poems/table/nabati/index.blade.php
+++ resources/views/pages/poems/table/nabati/index.blade.php
@@ -103,7 +103,7 @@ $dates = computed(function () {
                             },
                             formatCountdown: '00h 00m 00s',
                         }" x-init="initCountdown" x-show="showContent && currentDate=={{$loop->index}}" class="mt-4 text-2xl font-bold flex flex-col justify-center items-center">
-                            <h1 c>
+                            <h1>
                                 {{ !$item->is_break ? $item->owner : 'استراحة' }}
                             </h1>
 
@@ -128,58 +128,5 @@ $dates = computed(function () {
 
             </div>
         </div>
-
-    @script
-    function timer(expiry) {
-        return {
-            expiry: expiry,
-            remaining:null,
-            init() {
-            this.setRemaining()
-            setInterval(() => {
-                this.setRemaining();
-            }, 1000);
-            },
-            setRemaining() {
-            const diff = this.expiry - new Date().getTime();
-            this.remaining =  parseInt(diff / 1000);
-            },
-            days() {
-            return {
-                value:this.remaining / 86400,
-                remaining:this.remaining % 86400
-            };
-            },
-            hours() {
-            return {
-                value:this.days().remaining / 3600,
-                remaining:this.days().remaining % 3600
-            };
-            },
-            minutes() {
-                return {
-                value:this.hours().remaining / 60,
-                remaining:this.hours().remaining % 60
-            };
-            },
-            seconds() {
-                return {
-                value:this.minutes().remaining,
-            };
-            },
-            format(value) {
-            return ("0" + parseInt(value)).slice(-2)
-            },
-            time(){
-                return {
-                days:this.format(this.days().value),
-                hours:this.format(this.hours().value),
-                minutes:this.format(this.minutes().value),
-                seconds:this.format(this.seconds().value),
-            }
-            },
-        }
-        }
-    @endscript
     @endvolt
 @endsection
